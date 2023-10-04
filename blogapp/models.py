492aa7d985dfa1e2e878a8438146e46bea10b7ff
emekadefirst <emekadefirst@gmail.com>
2023-10-04 09:25:22 +0100

    auth added

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,5 +1,56 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, Group, Permission
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class AppUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('An email is required.')
+        if not password:
+            raise ValueError('A password is required.')
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None):
+        if not email:
+            raise ValueError('An email is required.')
+        if not password:
+            raise ValueError('A password is required.')
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class BlogAppUser(AbstractBaseUser, PermissionsMixin):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=50, unique=True)
+    username = models.CharField(max_length=50)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = AppUserManager()
+
+    # Add related_name to resolve the clash for groups and user_permissions
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name='blogapp_users_groups'
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='blogapp_users_permissions'
+    )
+
+    def __str__(self):
+        return self.username
 
 
 class Category(models.Model):
--- blogapp/serializers.py
+++ blogapp/serializers.py
@@ -1,5 +1,10 @@
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
 from .models import BlogPost, Comment
+from django.contrib.auth import get_user_model, authenticate
+
+UserModel = get_user_model()
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
@@ -15,3 +20,35 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = UserModel
+		fields = '__all__'
+
+	def create(self, clean_data):
+		user_obj = UserModel.objects.create_user(
+		    email=clean_data['email'], password=clean_data['password'])
+		user_obj.username = clean_data['username']
+		user_obj.save()
+		return user_obj
+
+
+class UserLoginSerializer(serializers.Serializer):
+	email = serializers.EmailField()
+	password = serializers.CharField()
+	##
+
+	def check_user(self, clean_data):
+		user = authenticate(
+		    username=clean_data['email'], password=clean_data['password'])
+		if not user:
+			raise ValidationError('user not found')
+		return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = UserModel
+		fields = ('email', 'username')
